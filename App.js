a69b3dba32249188f19397ef57449524aed77eac
Mateusz Morawiec <mateuszmorawiec98@wp.pl>
2020-03-16 21:33:01 +0100

    Devices states changing and notifications

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Navigator from './routes/homeStack';
 import { AppLoading } from 'expo';
--- screens/collectionScreen.js
+++ screens/collectionScreen.js
@@ -1,31 +1,97 @@
-import React, {useState} from 'react';
-import { StyleSheet, View, Text, FlatList, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, TouchableOpacity, Button } from 'react-native';
+import { notify, initnotify, getToken } from 'expo-push-notification-helper';
 import { globalStyles } from '../styles/global';
 import * as userDataUnparsed from '../data/userData.json';
 import CollectionCard from '../shared/CollectionCard';
 
 export default function Home({ navigation }) {
-    const [collectionData, setCollectionData] = useState(userDataUnparsed.user.collections);
-    
+    const [collections, setCollections] = useState(userDataUnparsed.user.collections);
+    const [refreshing, setRefreshing] = useState(false);
+
+    useEffect(() => {
+        // get collections from server
+    }, [refreshing]);
+
+    const refreshHandler = () => {
+        setRefreshing(true);
+    }
+
+    const findOpenAndNotify = () => {
+        let openList = collections.map(collection => {
+            return ({
+                collection: collection.collectionName,
+                devices: collection.devices.filter(device => {
+                    return !device.isClosed;
+                })
+            });
+        });
+        openList.forEach(collection => {
+            collection.devices.forEach(device => {
+                notification(collection.collection, `${device.deviceName} is open!`);
+            });
+        });
+        setRefreshing(false);
+    }
+
+    const notification = (title, body) => {
+        initnotify().then(async (data) => {
+            if (data) {
+                getToken().then((token) => {
+                    notify(token, title, body);
+                });
+            } else {
+                alert('Please, grant this app notification permission in settings.')
+            }
+        });
+    }
+
+    // -------- temporary functions --------
+
+    const pressHandler = () => {
+        findOpenAndNotify();
+    }
+
+    const changeState = (colId, devId) => {
+        let tempColData = collections;
+        for (let i = 0; i < tempColData.length; i++) {
+            if (tempColData[i]['colID'] === colId) {
+                for (let j = 0; j < tempColData[i]['devices'].length; j++) {
+                    if (tempColData[i]['devices'][j]['devID'] === devId) {
+                        tempColData[i]['devices'][j]['isClosed'] = !tempColData[i]['devices'][j]['isClosed'];
+                        break;
+                    }
+                }
+            }
+        }
+        setCollections(tempColData);
+    }
+
     return (
         <View style={{backgroundColor: '#a1e6e3', flex:1}}>
-        <View style={globalStyles.subheader}>
-            <Text style={globalStyles.textNotHighlighted}>Hello <Text style={globalStyles.textHighlight}>{userDataUnparsed.user.username}</Text>, here are your collections:</Text>
-        </View>
-        <View style={globalStyles.container}>
-        <FlatList 
-            keyExtractor={(item) => item.colID}
-            data={collectionData} 
-            renderItem={( {item} ) => (
-            <TouchableOpacity onPress={() => navigation.navigate('DeviceScreen', item)}>
-                <CollectionCard>
-                    <Text style={globalStyles.titleText}>{ item.collectionName }</Text>
-                    <Text>Devices: { item.numberOfDevices } | Last change: { item.lastStateChange}</Text>
-                </CollectionCard>
-            </TouchableOpacity>
-            )}
-        />
-        </View>
+            <View style={globalStyles.subheader}>
+                <Text style={globalStyles.textNotHighlighted}>Hello <Text style={globalStyles.textHighlight}>{userDataUnparsed.user.username}</Text>, here are your collections:</Text>
+                <Button 
+                    title='Trigger notification'
+                    onPress={pressHandler}
+                />               
+            </View>
+            <View style={globalStyles.container}>
+                <FlatList 
+                    keyExtractor={(item) => item.colID.toString()}
+                    data={collections}
+                    renderItem={({ item }) => (
+                        <TouchableOpacity onPress={() => navigation.navigate('DeviceScreen', {item, changeState})}>
+                            <CollectionCard>
+                                <Text style={globalStyles.titleText}>{ item.collectionName }</Text>
+                                <Text>Devices: {item.devices.length} | Last change: {item.lastStateChange}</Text>
+                            </CollectionCard>
+                        </TouchableOpacity>
+                    )}
+                    refreshing={refreshing}
+                    onRefresh={refreshHandler}
+                />
+            </View>
         </View>
     );
 }
