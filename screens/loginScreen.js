91e2c2a1ac1575659a48618d996f25e2896c7799
mibadura <mibadura@gmail.com>
2020-04-01 00:57:48 +0200

    Simple login screen

--- screens/loginScreen.js
+++ screens/loginScreen.js
@@ -1,14 +1,24 @@
 import React, { useState } from 'react';
-import { View, Text, Image, KeyboardAvoidingView, TouchableOpacity, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { View, Text, Image, KeyboardAvoidingView, TouchableOpacity, TouchableWithoutFeedback, Keyboard, TextInput } from 'react-native';
 import LoginForm from '../forms/loginForm';
 import RegisterForm from '../forms/registerForm';
 import { loginStyles } from '../styles/login';
 
 
 export default function LoginScreen() {
+
     const [register, setRegister] = useState(false);
+
+    // const credentials = {
+    //     username: '',
+    //     password: '',
+    // }
     
-    const handlePress = () => {
+    const handleLogin = () => {
+        setRegister(!register);
+    }
+
+    const handleSignin = () => {
         setRegister(!register);
     }
     
@@ -29,12 +39,33 @@ export default function LoginScreen() {
                     <View style={loginStyles.iconContainer}>
                         <Image source={require('../assets/doorime-logo.png')} style={loginStyles.icon} />
                     </View>
-                </KeyboardAvoidingView> 
-                {register ? (<RegisterForm />) : (<LoginForm />)}
-                <TouchableOpacity style={loginStyles.buttonContainer} onPress={handlePress}>
-                    <Text style={loginStyles.buttonText}>{register ? ('Login page') : ('Sign up')}</Text>
-                </TouchableOpacity>
+                </KeyboardAvoidingView>
+                <View style={loginStyles.inputContainer}>
+                    <TextInput
+                        //value={credentials.username}
+                        //onChangeText={(username) => this.setState({ username })}
+                        placeholder={'Username'}
+                        style={loginStyles.input}
+                        placeholderTextColor="#000" 
+                    />
+                    <TextInput
+                        //value={credentials.password}
+                        //onChangeText={(password) => this.setState({ password })}
+                        placeholder={'Password'}
+                        secureTextEntry={true}
+                        style={loginStyles.input}
+                        placeholderTextColor="#000" 
+                    />
+                </View> 
+                <View style={loginStyles.buttonContainer}>
+                    <TouchableOpacity style={loginStyles.buttonLogin} onPress={handleLogin}>
+                        <Text style={loginStyles.buttonText}>Log in</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={loginStyles.buttonSignin} onPress={handleSignin}>
+                        <Text style={loginStyles.buttonText}>Sign up</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         </TouchableWithoutFeedback>
-    )
+    );
 }
